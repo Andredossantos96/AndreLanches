6ace62692e932f72dcb9219087a36b38b03e6860
Andredossantos96 <86696902+Andredossantos96@users.noreply.github.com>
2022-03-22 08:29:26 -0300

    Models

---
+++ AndreLanches/Models/CategoriaModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AndreLanches.Models
+{
+    public class CategoriaModel
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+        public List<LancheModel> Lanches { get; set; }
+
+
+    }
+}
---
+++ AndreLanches/Models/LancheModel.cs
@@ -0,0 +1,20 @@
+﻿namespace AndreLanches.Models
+{
+    public class LancheModel
+    {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool Emestoque { get; set; }
+        public int CategoriaId { get; set; }
+        public virtual CategoriaModel Categoria { get; set; }
+
+
+
+    }
+}
