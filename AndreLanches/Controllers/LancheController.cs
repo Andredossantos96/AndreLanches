da566ec7446710c4f89721cf2f74c096e8fcc08b
Andredossantos96 <86696902+Andredossantos96@users.noreply.github.com>
2022-03-27 14:11:39 -0300

    LancheListViewModel

---
+++ AndreLanches/Controllers/LancheController.cs
@@ -0,0 +1,28 @@
+﻿using AndreLanches.Repositories.Interfaces;
+using AndreLanches.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AndreLanches.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult LancheList()
+        {
+            //var lanches = _lancheRepository.Lanches;
+            //return View(lanches);
+            var lanchesListViewModel = new LancheListViewModel();
+            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
+            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+
+            return View(lanchesListViewModel);
+
+        }
+    }
+}
---
+++ AndreLanches/ViewModels/LancheListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using AndreLanches.Models;
+
+namespace AndreLanches.ViewModels
+{
+    public class LancheListViewModel
+    {
+        public IEnumerable<LancheModel> Lanches { get; set; }
+        public string CategoriaAtual { get; set; }  
+    }
+}
